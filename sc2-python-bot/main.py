096b7fd0c97742e8e23fd05c835771e758842e93
Erik Halvarsson <eriha353@student.liu.se>
2018-11-05 18:22:05 +0100

    fixade uppgifter 1.1, 1.2, och 1.3 tillsammans med gruppen

--- sc2-python-bot/main.py
+++ sc2-python-bot/main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from typing import Optional
 from library import *
@@ -14,17 +15,57 @@ class MyAgent(IDABot):
     def on_step(self):
         IDABot.on_step(self)
         self.print_debug()
+        self.start_gathering()
 
     def print_debug(self):
         my_units = self.get_my_units()
-        base_location = self.base_location_manager.get_player_starting_base_location(0)
+        base_location = self.get_starting_base()
         my_minerals = base_location.minerals
         my_geysers = base_location.geysers
         for index, unit in enumerate(my_units+my_geysers+my_minerals):
-            type = unit.unit_type.name
-            id = unit.id
-            debug_string = "<UnitType: '{}'> id: {} i: {}".format(type, str(id), str(index))
+            unit_type = unit.unit_type.name
+            unit_id = unit.id
+            debug_string = "<UnitType: '{}'> id: {} i: {}".format(unit_type, str(unit_id), str(index))
             self.map_tools.draw_text(unit.position, debug_string, Color.WHITE)
+
+    def get_starting_base(self):
+        return self.base_location_manager.get_player_starting_base_location(PLAYER_SELF)
+
+    def get_my_workers(self):
+        workers = []
+        for unit in self.get_my_units():  # type: Unit
+            if unit.unit_type.is_worker:
+                workers.append(unit)
+
+        return workers
+
+    def start_gathering(self):
+        base_location = self.get_starting_base()
+        my_workers = self.get_my_workers()
+        base_minerals = base_location.minerals
+        for worker in my_workers:
+            if worker.is_idle:
+                choose_mineral = random.choice(base_minerals)
+                worker.right_click(choose_mineral)
+
+    def can_afford(self, unit_type: UnitType):
+        """ Returns True if there are an sufficient amount of minerals,
+        gas and supply to build the given unit_type, False otherwise """
+        return self.minerals >= unit_type.mineral_price\
+            and self.gas >= unit_type.gas_price\
+            and (self.max_supply - self.current_supply) >= unit_type.supply_required
+
+    def get_my_producers(self, unit_type: UnitType):
+        """ Returns a list of units which can build or train units of type unit_type """
+        producers = []
+        type_data = self.tech_tree.get_data(unit_type)
+        what_builds = type_data.what_builds
+
+        for unit in self.get_my_units():
+            if unit.unit_type in what_builds:
+                producers.append(unit)
+
+        return producers
 
 
 def main():
