0b0c005c6fc9c17511e47362013c75f6b58445f8
Jakob Telander <jakte528@student.liu.se>
2018-11-07 12:02:28 +0100

    Lade till ett condition i build_depots så bara en byggs i taget.

--- sc2-python-bot/main.py
+++ sc2-python-bot/main.py
@@ -41,14 +41,53 @@ class MyAgent(IDABot):
 
         return workers
 
-    def start_gathering(self):
-        base_location = self.get_starting_base()
+    def squared_distance(self, unit_1, unit_2):
+        p1 = unit_1.position
+        p2 = unit_2.position
+        return (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
+
+    def workers_working_in_base(self, base_location):
+        workers_working_in_base = 0
         my_workers = self.get_my_workers()
-        base_minerals = base_location.minerals
+        for workers in my_workers:
+            if self.squared_distance(workers, base_location) < 2 ** 2:
+                workers_working_in_base += 1
+        return workers_working_in_base
+
+    def get_workers_working_in_refinery(self, refinery):
+        my_workers = self.get_my_workers()
+        workers_working_in_refinery = []
         for worker in my_workers:
-            if worker.is_idle:
-                choose_mineral = random.choice(base_minerals)
-                worker.right_click(choose_mineral)
+            if refinery.is_completed and self.squared_distance(worker, refinery) < 2 ** 2:
+                workers_working_in_refinery.append(worker)
+        return workers_working_in_refinery
+
+    def start_gathering(self):
+        base_locations = self.base_location_manager.get_occupied_base_locations(PLAYER_SELF)
+        my_workers = self.get_my_workers()
+        for base in base_locations:
+            base_minerals = base.minerals
+            base_refineries = [self.get_refinery(geyser) for geyser in base.geysers] # TODO: Ändra från get_refinery, den returnerar None
+            for worker in my_workers:
+                if worker.is_idle:
+                    if self.workers_working_in_base(base) < 22:
+                        amount_of_gas_collectors = 0
+                        for refinery in base_refineries:
+                            amount_of_gas_collectors += len(self.get_workers_working_in_refinery(refinery))
+                        amount_of_mineral_collectors = self.workers_working_in_base(base) - amount_of_gas_collectors
+                        if amount_of_mineral_collectors < 16:
+                            chosen_mineral = random.choice(base_minerals)
+                            worker.right_click(chosen_mineral)
+                        else:
+                            for refinery in base_refineries:
+                                if len(self.get_workers_working_in_refinery(refinery)) < 3:
+                                    worker.right_click(refinery)
+
+
+
+
+
+
 
     def produce_workers(self):
         scv_type = UnitType(UNIT_TYPEID.TERRAN_SCV, self)
@@ -90,13 +129,9 @@ class MyAgent(IDABot):
     def is_worker_collecting_gas(self, worker):
         """ Returns: True if a Unit `worker` is collecting gas, False otherwise """
 
-        def squared_distance(unit_1, unit_2):
-            p1 = unit_1.position
-            p2 = unit_2.position
-            return (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
 
         for refinery in self.get_my_refineries():
-            if refinery.is_completed and squared_distance(worker, refinery) < 2 ** 2:
+            if refinery.is_completed and self.squared_distance(worker, refinery) < 2 ** 2:
                 return True
 
     def get_refinery(self, geyser: Unit) -> Optional[Unit]:
@@ -127,6 +162,18 @@ class MyAgent(IDABot):
             if len(constructing_workers) == 0:
                 worker = random.choice(workers)
                 worker.build(supply_depot, build_location)
+
+    def build_refineries(self):
+        refinery = UnitType(UNIT_TYPEID.TERRAN_REFINERY, self)
+        workers = self.get_my_workers()
+        constructing_workers = []
+        for worker in workers:
+            if worker.is_constructing(refinery):
+                constructing_workers.append(worker)
+        if self.current_supply > 25 and len(self.get_my_refineries()) < 2:
+            if self.can_afford(refinery) and constructing_workers == 0:
+                random.choice(workers).build_target(refinery, )
+
 
 def main():
     coordinator = Coordinator(r"D:\starcraft\StarCraft II\Versions\Base67188\SC2_x64.exe")
