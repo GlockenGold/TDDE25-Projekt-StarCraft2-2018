66ef95d6a3b5db2adb8f8587f8b8389feaa10d96
Erik Halvarsson <eriha353@student.liu.se>
2018-11-07 10:03:47 +0100

    fixade uppgift 1.4, partiell lösning till 1.5

--- sc2-python-bot/main.py
+++ sc2-python-bot/main.py
@@ -16,6 +16,8 @@ class MyAgent(IDABot):
         IDABot.on_step(self)
         self.print_debug()
         self.start_gathering()
+        self.produce_workers()
+        self.build_depots()
 
     def print_debug(self):
         my_units = self.get_my_units()
@@ -48,6 +50,24 @@ class MyAgent(IDABot):
                 choose_mineral = random.choice(base_minerals)
                 worker.right_click(choose_mineral)
 
+    def produce_workers(self):
+        scv_type = UnitType(UNIT_TYPEID.TERRAN_SCV, self)
+        my_workers = self.get_my_workers()
+        my_command_centers = self.get_my_producers(scv_type)
+        too_few = len(my_workers) < 16      # TODO: Ta bort hårdkodning, kolla hur många workers basen har, etc.
+        for center in my_command_centers:
+            if too_few and self.can_afford(scv_type) and not center.is_training:
+                center.train(scv_type)
+
+    def build_depots(self):
+        my_workers = self.get_my_workers()
+        supply_depot_type = UnitType(UNIT_TYPEID.TERRAN_SUPPLYDEPOT, self)
+        base_location = self.get_starting_base().depot_position
+        build_location = self.building_placer.get_build_location_near(base_location, supply_depot_type)
+        if self.current_supply >= self.max_supply - 1 and self.can_afford(supply_depot_type):
+            choose_worker = random.choice(my_workers)
+            choose_worker.build(supply_depot_type, build_location)
+
     def can_afford(self, unit_type: UnitType):
         """ Returns True if there are an sufficient amount of minerals,
         gas and supply to build the given unit_type, False otherwise """
@@ -66,6 +86,40 @@ class MyAgent(IDABot):
                 producers.append(unit)
 
         return producers
+
+    def get_my_refineries(self):
+        """ Returns a list of all refineries (list of Unit) """
+        refineries = []
+        for unit in self.get_my_units():
+            if unit.unit_type.is_refinery:
+                refineries.append(unit)
+        return refineries
+
+    def is_worker_collecting_gas(self, worker):
+        """ Returns: True if a Unit `worker` is collecting gas, False otherwise """
+
+        def squared_distance(unit_1, unit_2):
+            p1 = unit_1.position
+            p2 = unit_2.position
+            return (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
+
+        for refinery in self.get_my_refineries():
+            if refinery.is_completed and squared_distance(worker, refinery) < 2 ** 2:
+                return True
+
+    def get_refinery(self, geyser: Unit) -> Optional[Unit]:
+        """
+        Returns: A refinery which is on top of unit `geyser` if any, None otherwise
+        """
+
+        def squared_distance(p1: Point2D, p2: Point2D) -> float:
+            return (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
+
+        for unit in self.get_my_units():
+            if unit.unit_type.is_refinery and squared_distance(unit.position, geyser.position) < 1:
+                return unit
+
+        return None
 
 
 def main():
